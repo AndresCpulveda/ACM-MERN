847e21f7e36951749d8c6f368ad48e83c005a50f
Andres Sepulveda <andresfelipesepulveda@live.com>
2022-12-06 14:25:36 -0500

    duplicated docs removed

--- backend/helpers/generateId.js
+++
@@ -1,5 +0,0 @@
-const generateId = () => {
-  return Date.now().toString(32) + Math.random().toString(32).substring(2)
-}
-
-export default generateId
--- backend/helpers/generateJwt.js
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-const generateJwt = (id) => {
-  return jwt.sign({id}, process.env.JWT_KEY, {expiresIn: "5d"})
-}
-
-export default generateJwt
